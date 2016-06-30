774f4527049bbec6e5e9816d7271c2b4aa8a8d53
Cristiano Costa <engcristianoc@gmail.com>
2016-06-30 00:49:13 -0300

    Method to select language from url

--- pesquini/app/controllers/application_controller.rb
+++ pesquini/app/controllers/application_controller.rb
@@ -47,4 +47,9 @@ class ApplicationController < ActionController::Base
 
   end
 
+  before_filter :set_locale
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
 end
